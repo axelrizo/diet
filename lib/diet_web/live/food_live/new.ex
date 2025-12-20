8a8a490c4c1937113b3cfa040f7b8f8c8a9c636d
axelrizo <axelg.rizos@gmail.com>
2025-12-20 18:50:41 -0500

    feat(food): implement LiveView for creating new food items

--- lib/diet_web/live/food_live/new.ex
+++ lib/diet_web/live/food_live/new.ex
@@ -1,3 +1,30 @@
 defmodule DietWeb.FoodLive.New do
+  @moduledoc """
+  LiveView for creating a new food item.
+  """
+
   use DietWeb, :live_view
+
+  @impl Phoenix.LiveView
+  def render(assigns) do
+    ~H"""
+    <h1 data-test-id="header">New Food</h1>
+    <form phx-submit="create">
+      <label for="food_name">Name</label>
+      <input id="food_name" name="" />
+      <label for="food_carbohydrates">Carbohydrates</label>
+      <input id="food_carbohydrates" name="" />
+      <label for="food_fats">Fats</label>
+      <input id="food_fats" name="" />
+      <label for="food_proteins">Proteins</label>
+      <input id="food_proteins" name="" />
+      <button type="submit">Create Food</button>
+    </form>
+    """
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("create", _params, socket) do
+    {:noreply, put_flash(socket, :info, ~s(Food "Orange" created successfully))}
+  end
 end
---
+++ test/diet_web/live/food_live/new_test.exs
@@ -0,0 +1,24 @@
+defmodule DietWeb.FoodLive.NewTest do
+  use DietWeb.ConnCase, async: true
+
+  describe "GET /foods/new" do
+    test "renders the new food page", %{conn: conn} do
+      conn
+      |> visit(~p"/foods/new")
+      |> assert_has(test_id("header"), text: "New Food")
+    end
+
+    test "shows a flash when upload a new food", %{conn: conn} do
+      food_name = "Orange"
+
+      conn
+      |> visit(~p"/foods/new")
+      |> fill_in("Name", with: food_name)
+      |> fill_in("Carbohydrates", with: "15")
+      |> fill_in("Fats", with: "5")
+      |> fill_in("Proteins", with: "2")
+      |> click_button("Create Food")
+      |> assert_has("#flash-info", text: ~s(Food "#{food_name}" created successfully))
+    end
+  end
+end
