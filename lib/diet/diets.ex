1cc58d4a447cbb6f3a76a39e4f18f775af4e6a4a
axelrizo <axelg.rizos@gmail.com>
2026-01-03 01:09:05 -0500

    feat(diets): implement list_foods function and add corresponding tests

--- lib/diet/diets.ex
+++ lib/diet/diets.ex
@@ -1,9 +1,14 @@
 defmodule Diet.Diets do
   alias Diet.Diets.Food
+  alias Diet.Repo
+
+  def list_foods do
+    Repo.all(Food)
+  end
 
   def create_food(attrs) do
     %Food{}
     |> Food.changeset(attrs)
-    |> Diet.Repo.insert()
+    |> Repo.insert()
   end
 end
