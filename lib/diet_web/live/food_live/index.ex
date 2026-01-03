4bc577a840844fd3a74bed6a5c835a96d6236ddc
axelrizo <axelg.rizos@gmail.com>
2026-01-03 01:09:51 -0500

    feat(food): implement food list rendering in FoodLive.Index

--- lib/diet_web/live/food_live/index.ex
+++ lib/diet_web/live/food_live/index.ex
@@ -1,13 +1,22 @@
 defmodule DietWeb.FoodLive.Index do
   use DietWeb, :live_view
 
+  alias Diet.Diets
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign(:foods, Diets.list_foods())}
   end
 
   def render(assigns) do
     ~H"""
     <h1>Food Index</h1>
+    <.table id="foods-list" rows={@foods}>
+      <:col :let={food} label="id">{food.id}</:col>
+      <:col :let={food} label="name">{food.name}</:col>
+      <:col :let={food} label="proteins">{food.proteins}</:col>
+      <:col :let={food} label="fats">{food.fats}</:col>
+      <:col :let={food} label="carbohydrates">{food.carbohydrates}</:col>
+    </.table>
     """
   end
 end
---
+++ test/diet_web/live/food_live/index_test.exs
@@ -0,0 +1,17 @@
+defmodule DietWeb.FoodLive.IndexTest do
+  use DietWeb.ConnCase, async: true
+
+  alias DietWeb.FoodLive.Index
+
+  describe "GET /foods" do
+    test "renders the food list", %{conn: conn} do
+      foods = insert_list(2, :food)
+
+      conn
+      |> visit(~p"/foods")
+      |> assert_has("#foods-list tr", count: length(foods))
+      |> assert_has("#foods-list td", text: get_in(foods, [Access.at(0)]).name)
+      |> assert_has("#foods-list td", text: get_in(foods, [Access.at(1)]).name)
+    end
+  end
+end
