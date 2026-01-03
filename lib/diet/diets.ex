5067a768ed65367ca73f6ac54e2e796bca8d0baf
axelrizo <axelg.rizos@gmail.com>
2026-01-03 15:44:36 -0500

    feat(diets): enhance food management

--- lib/diet/diets.ex
+++ lib/diet/diets.ex
@@ -1,18 +1,104 @@
 defmodule Diet.Diets do
   @moduledoc """
   The Diets context.
+
+  This module provides functions for managing food items in the diet application.
   """
 
   alias Diet.Diets.Food
   alias Diet.Repo
 
+  @doc """
+  Returns the list of all foods.
+
+  ## Examples
+
+      iex> Diet.Diets.list_foods()
+      []
+
+  """
+  @spec list_foods() :: [Food.t()]
   def list_foods do
     Repo.all(Food)
   end
 
+  @doc """
+  Gets a single food by ID.
+
+  Raises `Ecto.NoResultsError` if the Food does not exist.
+
+  ## Examples
+
+      iex> {:ok, food} = Diet.Diets.create_food(%{name: "Apple", carbohydrates: 25, fats: 0.3, proteins: 0.5})
+      iex> Diet.Diets.get_food!(food.id).name
+      "Apple"
+
+  """
+  @spec get_food!(integer()) :: Food.t()
+  def get_food!(id) do
+    Repo.get!(Food, id)
+  end
+
+  @doc """
+  Creates a food with the given attributes.
+
+  ## Examples
+
+      iex> {:ok, %Diet.Diets.Food{} = food} = Diet.Diets.create_food(%{name: "Banana", carbohydrates: 23, fats: 0.3, proteins: 1.1})
+      iex> food.name
+      "Banana"
+
+      iex> {:error, %Ecto.Changeset{}} = Diet.Diets.create_food(%{name: nil})
+
+  """
+  @spec create_food(map()) :: {:ok, Food.t()} | {:error, Ecto.Changeset.t()}
   def create_food(attrs) do
     %Food{}
     |> Food.changeset(attrs)
     |> Repo.insert()
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking food changes.
+
+  This is typically used in forms to track changes before persisting them.
+
+  ## Examples
+
+      iex> food = %Diet.Diets.Food{name: "Orange", carbohydrates: Decimal.new("20"), fats: Decimal.new("0.2"), proteins: Decimal.new("1.5")}
+      iex> changeset = Diet.Diets.change_food(food)
+      iex> changeset.data.name
+      "Orange"
+
+      iex> food = %Diet.Diets.Food{name: "Orange", carbohydrates: Decimal.new("20"), fats: Decimal.new("0.2"), proteins: Decimal.new("1.5")}
+      iex> changeset = Diet.Diets.change_food(food, %{name: "Grape"})
+      iex> changeset.changes.name
+      "Grape"
+
+  """
+  @spec change_food(Food.t(), map()) :: Ecto.Changeset.t()
+  def change_food(%Food{} = food, attrs \\ %{}) do
+    Food.changeset(food, attrs)
+  end
+
+  @doc """
+  Updates a food with the given attributes.
+
+  ## Examples
+
+      iex> {:ok, food} = Diet.Diets.create_food(%{name: "Mango", carbohydrates: 15, fats: 0.4, proteins: 0.8})
+      iex> {:ok, updated} = Diet.Diets.update_food(food, %{name: "Updated Mango"})
+      iex> updated.name
+      "Updated Mango"
+
+      iex> {:ok, food} = Diet.Diets.create_food(%{name: "Pear", carbohydrates: 15, fats: 0.1, proteins: 0.4})
+      iex> {:error, %Ecto.Changeset{}} = Diet.Diets.update_food(food, %{name: nil})
+
+  """
+  @spec update_food(Food.t(), map()) :: {:ok, Food.t()} | {:error, Ecto.Changeset.t()}
+  def update_food(%Food{} = food, attrs) do
+    food
+    |> Food.changeset(attrs)
+    |> Repo.update()
   end
 end
--- test/diet/diets_test.exs
+++ test/diet/diets_test.exs
@@ -1,11 +1,13 @@
 defmodule Diet.DietsTest do
   use Diet.DataCase, async: true
 
+  doctest Diet.Diets
+
   alias Diet.Diets
   alias Diet.Diets.Food
 
   describe "create_food/1" do
-    test "returns food and inserts it" do
+    test "returns food and inserts it when given valid attributes" do
       attrs = params_for(:food)
 
       assert {:ok, %Food{}} = Diets.create_food(attrs)
@@ -20,6 +22,60 @@ defmodule Diet.DietsTest do
       assert returned_foods = Diets.list_foods()
       for food <- foods, do: %Food{} |> match?(food) |> assert()
       assert length(returned_foods) == length(foods)
+    end
+  end
+
+  describe "get_food!/1" do
+    test "returns the food when it exists" do
+      food = insert(:food)
+
+      assert %Food{} = returned_food = Diets.get_food!(food.id)
+      assert returned_food.id == food.id
+      assert returned_food.name == food.name
+    end
+
+    test "raises Ecto.NoResultsError when food does not exist" do
+      assert_raise Ecto.NoResultsError, fn ->
+        Diets.get_food!(999_999)
+      end
+    end
+  end
+
+  describe "change_food/2" do
+    test "returns a food changeset when a food struct" do
+      food = insert(:food)
+
+      assert %Ecto.Changeset{} = changeset = Diets.change_food(food)
+      assert changeset.data == food
+    end
+
+    test "returns a food changeset when attributes" do
+      food = insert(:food)
+      new_attrs = params_for(:food, name: "New Food Name")
+
+      assert %Ecto.Changeset{} = changeset = Diets.change_food(food, new_attrs)
+      assert changeset.changes.name == "New Food Name"
+    end
+  end
+
+  describe "update_food/2" do
+    test "updates the food when valid attributes" do
+      food = insert(:food)
+      update_attrs = params_for(:food, name: "Updated Food")
+
+      assert {:ok, %Food{} = updated_food} = Diets.update_food(food, update_attrs)
+      assert updated_food.name == "Updated Food"
+      assert Decimal.eq?(updated_food.carbohydrates, update_attrs.carbohydrates)
+      assert Decimal.eq?(updated_food.fats, update_attrs.fats)
+      assert Decimal.eq?(updated_food.proteins, update_attrs.proteins)
+    end
+
+    test "returns error changeset when invalid attributes" do
+      food = insert(:food)
+      invalid_attrs = %{name: nil, carbohydrates: nil, fats: nil, proteins: nil}
+
+      assert {:error, %Ecto.Changeset{} = changeset} = Diets.update_food(food, invalid_attrs)
+      assert "can't be blank" in errors_on(changeset).name
     end
   end
 end
